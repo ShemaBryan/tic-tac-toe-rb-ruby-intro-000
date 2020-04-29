abb8bd298c79b98a02078ac7e217579c16b6e1b1
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-29 22:50:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,11 +78,9 @@ end
 def draw?(board)
    if !won?(board) && full?(board)
      return true
-   elsif !won?(board) && !full?(board)
-     return false
-   else won?(board)
-     return false
-   end
+   else
+  return false
+  end
 end
 
 def over?(board)
