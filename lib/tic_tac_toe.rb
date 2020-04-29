92328b49263aae2f0f2f2925dd4646fc144677df
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-29 22:15:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ def position_taken?(board, index)
    end
 end
 
-def valid_move?(board, index, player)
+def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index, player)
       return true
    end
